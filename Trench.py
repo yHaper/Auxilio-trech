0c790c47f0e092af37a4e59a70647ec0127ade3e
yHaper <mlopesdossantos550@gmail.com>
2021-03-14 15:20:33 -0200

    first commit

---
+++ Trench.py
@@ -0,0 +1,59 @@
+import os
+
+os.system("clear")
+
+print(" Ａｕｘｉｌｉｏ    ｔｒｅｎｃｈ  𓅐\n      ʸᴴᵃᵖᵉʳ     \n")
+
+Nome = input("\033[0;37m𝘚𝘦𝘶 𝘯𝘰𝘮𝘦: \033[m")
+
+os.system("clear")
+
+print(" \033[1;33mＡｕｘｉｌｉｏ    ｔｒｅｎｃｈ  𓅐\033[m\n      \033[0;37mʸᴴᵃᵖᵉʳ\033[m     \n")
+
+print("\033[0;33m         Ola {} \n \033[m" .format(Nome))
+
+print("""\033[0;33m
+• \033[0;37mIniciar \033[0;33m
+• \033[0;37mCreditos \033[0;37m\033[m \033[0;33m
+
+ 
+ \033[m  """)
+trench = input("\033[0;33m~ \033[m")
+os.system("clear")
+
+if trench == 'Iniciar' or trench == 'iniciar' or trench == 'i' or trench == 'I' or trench == 'start' or trench == 'Start':
+	
+	print(" \033[1;33mＡｕｘｉｌｉｏ    ｔｒｅｎｃｈ  𓅐\033[m\n     𝘘𝘶𝘢𝘭 𝘧𝘦𝘳𝘳𝘢𝘮𝘦𝘯𝘵𝘢 𝘥𝘦𝘴𝘦𝘫𝘢 𝘪𝘯𝘴𝘵𝘢𝘭𝘢𝘳    \n")
+	print("""• yHaper - IP\n• Calculadora de burros\n• yHaper - Tool\n•\n•\n• Sair\n\n""")
+	trench = input("\033[0;33m~ \033[m")
+	
+elif trench == 'yHaper - IP' or trench == 'yhaper' or trench == 'YHAPER' or trench == 'yhaper-ip' or trench == 'YHAPER-IP' or trench == 'YHAPER - IP' or trench == 'yhaper - ip' or trench == 'ip' or trench == 'IP' or trench == 'IP = yHaper' or trench == 'yHaper':
+   print("\033[0;33mBaixando...\033[m")
+   os.system("clear")
+   os.system("git clone https://github.com/yHaper/yHaper-IP")
+   os.system("clear")
+   
+elif trench == 'Calculadora de burros' or trench == 'CALCULADORA DE BURROS' or trench == 'calculadora de burros' or trench == 'Calculadora' or trench == 'CALCULADORA' or trench == 'calculadora' or trench == 'calc' or trench == 'CALC' or trench == 'Calc' or trench == 'BURRO' or trench == 'burro' or trench == 'Burro' or trench == 'buro' or trench == 'BURO':
+	print("\033[0;33mBaixando...\033[m")
+	os.system("clear")
+	os.system("git clone https://github.com/yHaper/yHaper-Calc")
+	os.system("clear")
+	
+elif trench == 'yhaper-tool' or trench == 'YHAPER-TOOL' or trench == 'yHaper-Tool' or trench == 'yHaper - tool' or trench == 'yHaper - Tool' or trench == 'Tool' or trench == 'TOOL' or trench == 'T' or trench == 't' or trench == 'YHAPER - TOOL' or trench == 'To':
+	print("\033[0;33mBaixando...\033[m")
+	os.system("clear")
+	os.system("git clone https://github.com/yHaper/yHaper-Tool")
+	os.system("clear")
+
+   
+
+   
+
+	   	
+if trench == 'Sair' or trench == 'S' or trench == 's' or trench == 'quit' or trench == 'QUIT' or trench == 'q' or trench == 'Q' or trench == 'sair':
+	print("\033[0;33mVolte sempre\033[m")
+
+if trench == 'Creditos' or trench == 'creditos' or trench == 'cre' or trench == 'creotos' or trench == 'C' or trench == 'c':
+   	 os.system("clear")
+   	 print("\033[0;32mＣｒｅｄｉｔｏｓ  𓆗\n    \033[0;37m\033[m\n\n\n")
+   	 print("\033[0;32mＩｎｓｔａｇｒａｍ : \033[0;37myhaper_\n\033[0;32mＹｏｕｔｕｂｅ : \033[0;37myHaper 1.6\033[m")
